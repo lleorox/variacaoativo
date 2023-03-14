966b77767df1e1019fe1e6b28a4bb7adfa86f5a5
lleorox <leonardodgl@gmail.com>
2023-03-14 10:24:00 -0300

    Create AtivosController1.cs

---
+++ AtivosController1.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using VariacaoAtivo.Models;
+
+
+namespace VariacaoAtivo.Controllers
+{
+
+    [ApiController]
+    [Route("ativos")]
+    public class AtivosController : ControllerBase
+    {
+        private readonly MySqlConnection _connection;
+
+        public AtivosController(MySqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        [HttpGet("{nome}")]
+        public async Task<ActionResult<IEnumerable<Ativos>>> Get(string nome)
+        {
+            var ativos = await _connection.QueryAsync<Ativos>(
+                "SELECT * FROM ativos WHERE nome = @Nome ORDER BY data DESC LIMIT 30",
+                new { Nome = nome });
+
+            var ativosList = ativos.ToList();
+
+            for (int i = 0; i < ativosList.Count - 1; i++)
+            {
+                ativosList[i].Variacao = ativosList[i].Valor - ativosList[i + 1].Valor;
+            }
+
+            return ativosList;
+        }
+    }
+
+}
